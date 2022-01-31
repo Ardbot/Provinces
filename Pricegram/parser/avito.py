3d4db2616525ed609b8ecf9f0fc94404c41a40d6
Ardbot <45563576+Ardbot@users.noreply.github.com>
2022-01-31 16:12:11 +0900

    Правка

--- Pricegram/parser/avito.py
+++ Pricegram/parser/avito.py
@@ -2,10 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 
 if __name__ == '__main__':
-    region = "amurskaya_oblast"
+    region = "/amurskaya_oblast"
+    categories = "/tovary_dlya_kompyutera/"
     product = "Видеокарты"
 
+    # url = f"https://www.avito.ru/{region}{categories}?q={product}"
     url = f"https://www.avito.ru/{region}?q={product}"
+    print(url)
 
     requests = requests.get(url)
     bs = BeautifulSoup(requests.text, "html.parser")
@@ -13,8 +16,4 @@ if __name__ == '__main__':
     all_links = bs.find_all('a', class_="iva-item-title-py3i_")
     for link in all_links:
         print("https://www.avito.ru" + link["href"])
-
-
-
-
 
