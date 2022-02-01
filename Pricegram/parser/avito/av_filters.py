d54b17590e556c1861411a06280b5841ea52598e
Ardbot <45563576+Ardbot@users.noreply.github.com>
2022-02-01 15:54:32 +0900

    Создание фильтра

--- Pricegram/parser/avito/av_filters.py
+++ Pricegram/parser/avito/av_filters.py
@@ -6,20 +6,16 @@ class Filters(object):
     def __init__(self, region, sity, categories, subcategories, product):
         self.region = region
         self.sity = sity
-        # self.sity = "belogorsk"
         self.categories = categories
         self.subcategories = subcategories
         self.product = product
 
     def stringr_url(self):
-        # url = f"https://www.avito.ru/{region}{categories}?q={product}"
-        # https: // www.avito.ru / amurskaya_oblast / tovary_dlya_kompyutera?cd = 1
-        url = f"https://www.avito.ru/{self.region}_{self.sity}/{self.categories}/{self.subcategories}?q={self.product}"
-        print(url)
+        url = f"https://www.avito.ru/{self.region}/{self.sity}/{self.categories}/{self.subcategories}?q={self.product}"
         return url
-
-    print(stringr_url())
 
 
 if __name__ == '__main__':
-    pass
+    run = Filters("rossiya", "tovary_dlya_kompyutera", "komplektuyuschie", "videokarty", "Видеокарта")
+    print(run.sity)
+    print(run.stringr_url())
