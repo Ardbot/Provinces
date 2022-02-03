7fd5cdb5150e5dd5d453e6656e25d8273f4d3403
Ardbot <45563576+Ardbot@users.noreply.github.com>
2022-02-04 01:57:11 +0900

    Парсинг названия, цены, города, ссылки

--- Pricegram/parser/avito/avito.py
+++ Pricegram/parser/avito/avito.py
@@ -2,15 +2,40 @@ from bs4 import BeautifulSoup
 import requests
 from av_filters import Filters
 
-def test(requests=None):
 
-    url = Filters.stringr_url()
-    print(url)
-    requests = requests.get(url)
-    bs = BeautifulSoup(requests.text, "html.parser")
+def test():
+    # filter = Filters("amurskaya_oblast", "tovary_dlya_kompyutera", "komplektuyuschie", "videokarty", "rtx 3050")
+    # url = filter.stringr_url()
+    # print(url)
+    url = "https://www.avito.ru/amurskaya_oblast/tovary_dlya_kompyutera/komplektuyuschie/videokarty-ASgBAgICAkTGB~pm7gmmZw?p=1"
+    r = requests.get(url)
+    bs = BeautifulSoup(r.text, "html.parser")
+    all_content = bs.find_all('div', class_="iva-item-content-rejJg")
 
-    all_links = bs.find_all('a', class_="iva-item-title-py3i_")
-    for link in all_links:
-        print("https://www.avito.ru" + link["href"])
+
+    # for link in all_content:
+    #     print(link)
+    #     print("https://www.avito.ru" + link["href"])
 
 
+    # print(all_links)
+
+    for content in all_content:
+        name = content.find('h3', class_="title-root-zZCwT")
+        print(name.text)
+
+        price = content.find(class_="price-text-_YGDY")
+        print(price.text)
+
+        sity = content.find(class_="geo-georeferences-SEtee")
+        print(sity.text)
+
+        url_product = content.find(class_="iva-item-titleStep-pdebR")
+        for link in url_product:
+            print("https://www.avito.ru" + link["href"])
+
+        print("#" * 20)
+
+
+if __name__ == '__main__':
+    test()
